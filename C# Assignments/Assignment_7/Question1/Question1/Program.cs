7a5b013128b86e0fe705cd4df3d68652a3eea8b4
Kerkar <rajdeep.kerkar@capgemini.com>
2022-12-27 13:30:01 +0530

    Assignment-7 Question-1

---
+++ C# Assignments/Assignment_7/Question1/Question1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\RAKERKAR\.Net-FullStack\C# Assignments\Assignment_7\Question1";
+
+            var dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            var files = Directory.GetFiles(rootPath, "*.txt", SearchOption.AllDirectories);
+
+            Console.WriteLine("-----Displaying directories-----");
+            foreach(string dir in dirs)
+            {
+                var dirInfo = new DirectoryInfo(dir);
+                Console.WriteLine($"{dirInfo.FullName}");
+            }
+            Console.WriteLine("\n-----Displaying all .txt files-----");
+            foreach (string file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                Console.WriteLine($"{Path.GetFileName(file)} - { fileInfo.Length } bytes");
+            }
+        }
+    }
+}
