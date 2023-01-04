8cb571f57c0ddd223f8e9d0885340068a8ce1d3d
Kerkar <rajdeep.kerkar@capgemini.com>
2023-01-04 17:25:15 +0530

    SQL server assignment-3

---
+++ C# Assignments/Assignment_8/Question1/TestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Question1;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
